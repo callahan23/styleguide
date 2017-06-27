6e247eb234a8cf3adfe32886a385a60cac889348
Dirk Eremberg <dropkick23@gmx.de>
2017-06-27 20:21:19 +0200

    add gruntfile.js, edit .gitignore

---
+++ gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-kss');
+
+    var env = grunt.option('env') || 'dev';
+
+    grunt.initConfig({
+
+        watch : {
+            assets : {
+                files :   [
+                    'src/**', '!**/*.*'
+
+                ],
+			tasks :   [ 'copy:assets', 'kss' ],
+                options : {
+                    livereload : true
+                }
+            },
+
+            styles_kss : {
+                files :   [
+                    'src/**/*.css', 'src/css/**/*.html'
+                ],
+                tasks :   [ 'kss' ],
+                options : {
+                    livereload : true
+                }
+            }
+        },
+
+        copy : {
+            assets : {
+                files : [
+                    {
+                        expand : true,
+                        cwd :    'src',
+                        src :    [
+                            '**/*'
+                        ],
+                        dest :   'web'
+                    }
+                ]
+            }
+        },
+
+        kss : {
+            options : {
+                verbose : true,
+                css: '../web/css/style.css'
+            },
+            dist :    {
+                src :  ['src/css'],
+                dest : 'styleguide'
+            }
+        }
+    });
+
+    // Build Tasks
+    var buildEssentialTasks = [ 'copy' ];
+    
+    grunt.registerTask('build_essentials', buildEssentialTasks);
+    grunt.registerTask('build_complete', [ 'build_essentials', 'kss' ]);
+
+    // Dev Tasks
+    grunt.registerTask('dev', function () {
+        delete grunt.config.data.watch.styles_kss;
+        grunt.task.run([ 'build_essentials', 'watch' ]);
+    });
+    grunt.registerTask('dev_complete', [ 'build_complete', 'watch' ]);
+
+    // Default Task => build_complete
+    grunt.registerTask('default', [ 'build_complete' ]);
+};
