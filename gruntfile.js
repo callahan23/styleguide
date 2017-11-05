5534ed7982aa00db993b9d01a2f6b5db93965395
Dirk Eremberg <dropkick23@gmx.de>
2017-11-05 15:23:03 +0100

    add comments to gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -1,49 +1,69 @@
-
+// Grunt ist der im Projekt vorgegebene Taskrunner. In dieser Datei werden
+// die einzelnen Tasks definiert, die Grunt automatisch, z.B. bei Änderungen
+// des Quellcodes ausführt.
 module.exports = function (grunt) {
-
+    // Lade über npm installierte "PlugIns", die die Grundfunktionalität von
+    // Grunt erweitern.
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    // macht Grunt less-fähig.
+    // Im Projekt wird less verwendet, das für den Browser zu css kompiliert
+    // wird. less ist eine Erweiterung zu css, die z.B. eine Verwendung von
+    // Variablen, Mixins, etc. ermöglicht.
     grunt.loadNpmTasks('grunt-contrib-less');
+    // erweitert Grunt um die Fähigkeit automatisch den Styleguide Generator
+    // kss auszuführen.
     grunt.loadNpmTasks('grunt-kss');
-
+    // "Umgebungsvariable" um unterschiedliche Tasks für Production und
+    // Development ausführen zu können (wird derzeit noch nicht verwendet)
     var env = grunt.option('env') || 'dev';
 
+    // initialisiere Grunt Tasks:
     grunt.initConfig({
-
+        // Überwache Dateisystem auf Änderungen
         watch : {
+              // bei Änderungen an less Dateien starte less task (siehe unten)
               less: {
                   files: ['src/**/*.less'],
                   tasks: ['less']
               },
+              // bei Änderungen der Ordnerstruktur im Sourcedirectory starte
+              // Copy - und kss Task.
             assets : {
                 files :   [
                     'src/**',
                     '!**/*.*',
                 ],
-		tasks :   [ 'copy:assets', 'kss' ],
+                tasks :   [ 'copy:assets', 'kss' ],
                 options : {
+                    // wird bisher noch nicht verwendet
                     livereload : true
                 }
             },
+            // bei Änderungen an less- oder HTML Files starte kss Task
             styles_kss : {
                 files :   [
                     'src/**/*.less', 'src/less/**/*.html'
                 ],
                 tasks :   [ 'kss' ],
                 options : {
+                    // wird bisher noch nicht verwendet
                     livereload : true
                 }
             }
         },
+            // less Task: verwende alle less Files im Sourcedirectory und
+            // kompiliere in eine einzige css Datei.
             less: {
                 files: {
                     src: 'src/**/*.less',
                     dest: 'web/css/index.css'
                 }
             },
-
-        copy : {
+            // copy Task: kopiere bei Änderungen im Dateisystem alle Dateien
+            // außer less Dateien ins web Verzeichnis.
+            copy : {
             assets : {
                 files : [
                     {
@@ -58,7 +78,11 @@ module.exports = function (grunt) {
                 ]
             }
         },
-
+        // kss Task: rufe kss auf, mit folgenden zusätzlichen Argumenten:
+        // -verbose: ausführliches Logging
+        // -css: das zu verwendende Stylesheet
+        // -title: Seitentitel für die Styleguide Seite
+        // -builder: Pfad zum zu verwendenden Styleguide-Builder
         kss : {
             options : {
                 verbose : true,
@@ -67,6 +91,8 @@ module.exports = function (grunt) {
                 builder: 'styleguide-theme'
 
             },
+            // -src: die, für die Styleguidegenerierung verwendeten less-Dateien
+            // -dest: Pfad, wo der Styleguide hin generiert wird
             dist :    {
                 src :  ['src/less'],
                 dest : 'styleguide'
@@ -74,6 +100,7 @@ module.exports = function (grunt) {
         }
     });
 
+    // Verschiedene Möglichkeiten Grunt aufzurufen:
     // Build Tasks
     var buildEssentialTasks = [ 'copy' ];
 
